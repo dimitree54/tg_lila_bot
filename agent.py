1f8f37eb57dcc36aa084c9217ec94a00ff8f7a4b
my_mac_dimitree54 <dimitree54@gmail.com>
2023-06-11 18:10:26 +0300

    multi-user support + saving memory on disk (to survive reboot)

--- agent.py
+++ agent.py
@@ -1,14 +1,42 @@
-from dotenv import load_dotenv
+import os.path
+
 from langchain.agents import initialize_agent, AgentType
 from langchain.chat_models import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
 
-load_dotenv()
 
-agent = initialize_agent(
-    tools=[],
-    llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0),
-    verbose=True,
-    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
-    memory=ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-)
+class Lila:
+    def __init__(self, save_path: str):
+        self.save_path = save_path
+        if not os.path.isdir(save_path):
+            os.makedirs(save_path)
+
+    def _load_memory(self, user_id: int) -> ConversationBufferMemory:
+        memory_path = os.path.join(self.save_path, f"{user_id}.json")
+        return ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True,
+            chat_memory=FileChatMessageHistory(memory_path)
+        )
+
+    def forget(self, user_id: int):
+        memory = self._load_memory(user_id=user_id)
+        memory.clear()
+
+    @staticmethod
+    def _initialise_agent(memory: ConversationBufferMemory):
+        return initialize_agent(
+            tools=[],
+            llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0),
+            verbose=True,
+            agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+            memory=memory
+        )
+
+    async def arun(self, user_id: int, request: str) -> str:
+        try:
+            memory = self._load_memory(user_id=user_id)
+            agent = self._initialise_agent(memory=memory)
+            answer = await agent.arun(input=request)
+            return answer
+        except Exception as e:
+            return str(e)
--- tg_bot.py
+++ tg_bot.py
@@ -3,35 +3,23 @@ import tempfile
 
 import openai
 from dotenv import load_dotenv
-from langchain.agents import AgentExecutor
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, CallbackContext, filters, CommandHandler
 
-from agent import agent
+from agent import Lila
 from utils import ogg_to_mp3, mp3_to_text, text_to_mp3_multi_language
 
 
 class TelegramBot:
-    def __init__(self, token: str):
+    def __init__(self, token: str, lila: Lila):
         self.application = ApplicationBuilder().token(token=token).build()
         self.application.add_handler(CommandHandler("forget", self.command_handler))
         self.application.add_handler(MessageHandler(filters.VOICE, self.voice_handler))
         self.application.add_handler(MessageHandler(filters.TEXT, self.text_handler))
-        self.agent: AgentExecutor = agent
+        self.lila = lila
 
     def run_polling(self):
         self.application.run_polling()
-
-    def _forget(self):
-        self.agent.memory.clear()
-
-    async def _agent_call(self, request: str) -> str:
-        try:
-            return await self.agent.arun(
-                input=request
-            )
-        except Exception as e:
-            return str(e)
 
     @staticmethod
     async def _load_voice_mp3(update: Update, context: CallbackContext, mp3_path: str):
@@ -44,19 +32,19 @@ class TelegramBot:
         with tempfile.NamedTemporaryFile(suffix=".mp3") as mp3_file:
             await self._load_voice_mp3(update, context, mp3_file.name)
             transcript = mp3_to_text(mp3_file.name)
-            answer = await self._agent_call(transcript)
+            answer = await self.lila.arun(update.message.from_user.id, transcript)
             if text_to_mp3_multi_language(answer, mp3_file.name) is None:
                 await update.message.reply_text(answer)
             else:
                 await update.message.reply_voice(voice=mp3_file.name, caption=answer)
 
     async def text_handler(self, update: Update, context: CallbackContext) -> None:  # noqa
-        answer = await self._agent_call(update.message.text)
+        answer = await self.lila.arun(update.message.from_user.id, update.message.text)
         await update.message.reply_text(answer)
 
     async def command_handler(self, update: Update, context: CallbackContext) -> None:  # noqa
         if update.message.text == "/forget":
-            self._forget()
+            self.lila.forget(update.message.from_user.id)
             await update.message.reply_text("Chat history has been forgotten.")
         else:
             await update.message.reply_text("Unknown command.")
@@ -65,4 +53,7 @@ class TelegramBot:
 if __name__ == '__main__':
     load_dotenv()
     openai.api_key = os.environ["OPENAI_API_KEY"]
-    TelegramBot(token=os.environ["TELEGRAM_TOKEN"]).run_polling()
+    TelegramBot(
+        token=os.environ["TELEGRAM_TOKEN"],
+        lila=Lila(save_path=os.environ["SAVE_PATH"])
+    ).run_polling()
