46da5cc86e0f2029b48c28d8f80746614a64cc9d
my_mac_dimitree54 <dimitree54@gmail.com>
2023-06-16 22:46:01 +0300

    move savable memory to separate class

---
+++ memory.py
@@ -0,0 +1,87 @@
+import json
+import os
+from datetime import datetime
+from typing import List, Dict, Any
+
+from langchain.memory import ConversationSummaryBufferMemory, ChatMessageHistory
+from langchain.schema import BaseMessage, messages_from_dict, messages_to_dict
+
+
+class ChatMessageHistoryWithDates(ChatMessageHistory):
+    def add_message(self, message: BaseMessage) -> None:
+        message.additional_kwargs["timestamp"] = datetime.now()
+        super().add_message(message)
+
+
+class SavableSummaryBufferMemoryWithDates(ConversationSummaryBufferMemory):
+    save_path: str
+    chat_memory_file_name: str
+    summary_memory_file_name: str
+
+    @classmethod
+    def load(cls, save_path: str, **kwargs):
+        chat_memory_file_name = kwargs.pop("chat_memory_file_name", "chat_memory.json")
+        summary_memory_file_name = kwargs.pop("summary_memory_file_name", "summary_memory.json")
+        chat_memory_path = os.path.join(save_path, chat_memory_file_name)
+        summary_memory_path = os.path.join(save_path, summary_memory_file_name)
+        chat_memory = cls._load_chat_memory(chat_memory_path)
+        summary_memory = cls._load_summary_memory(summary_memory_path)
+        return cls(
+            save_path=save_path,
+            chat_memory_file_name=chat_memory_file_name,
+            summary_memory_file_name=summary_memory_file_name,
+            chat_memory=chat_memory, moving_summary_buffer=summary_memory, **kwargs
+        )
+
+    @staticmethod
+    def _load_messages(save_path: str) -> List[BaseMessage]:
+        with open(save_path, "r") as f:
+            items = json.load(f)
+        messages = messages_from_dict(items)
+        return messages
+
+    @staticmethod
+    def _load_chat_memory(chat_memory_path: str) -> ChatMessageHistory:
+        if os.path.isfile(chat_memory_path):
+            messages = SavableSummaryBufferMemoryWithDates._load_messages(chat_memory_path)
+            chat_memory = ChatMessageHistory(messages=messages)
+        else:
+            chat_memory = ChatMessageHistory()
+        return chat_memory
+
+    @staticmethod
+    def _load_summary_memory(summary_memory_path: str) -> str:
+        if os.path.isfile(summary_memory_path):
+            with open(summary_memory_path, "r") as f:
+                summary_memory = f.read()
+        else:
+            summary_memory = ""
+        return summary_memory
+
+    @staticmethod
+    def _save_messages(messages: List[BaseMessage], save_path: str):
+        messages_dict = messages_to_dict(messages)
+        with open(save_path, "w") as f:
+            json.dump(messages_dict, f)
+
+    def _save_chat_memory(self):
+        chat_memory_path = os.path.join(self.save_path, self.chat_memory_file_name)
+        messages = self.chat_memory.messages
+        self._save_messages(messages, chat_memory_path)
+
+    def _save_summary_memory(self):
+        summary_memory_path = os.path.join(self.save_path, self.summary_memory_file_name)
+        with open(summary_memory_path, "w") as f:
+            f.write(self.moving_summary_buffer)
+
+    def save(self):
+        self._save_chat_memory()
+        self._save_summary_memory()
+
+    def save_context(self, inputs: Dict[str, Any], outputs: Dict[str, str]) -> None:
+        super().save_context(inputs, outputs)
+        self.save()
+
+    def clear(self) -> None:
+        super().clear()
+        self.save()
--- tests/test_agent.py
+++ tests/test_agent.py
@@ -3,17 +3,18 @@ import tempfile
 from unittest import TestCase
 
 from dotenv import load_dotenv
+from langchain.llms import FakeListLLM
 from langchain.memory.chat_memory import BaseChatMemory
 
-from agent import Lila
+from memory import SavableSummaryBufferMemoryWithDates
 
 
 class TestMemory(TestCase):
     def setUp(self) -> None:
         load_dotenv()
         self.save_path = tempfile.mkdtemp()
-        self.agent = Lila(self.save_path)
         self.test_user_id = 0
+        self.fake_summary = "blah blah blah"
 
     @staticmethod
     def add_test_messages(memory: BaseChatMemory):
@@ -21,36 +22,47 @@ class TestMemory(TestCase):
         memory.save_context({"input": "not much you"}, {"output": "not much"})
         memory.save_context({"input": "bye"}, {"output": "see you"})
 
+    def load_memory(self, token_limit: int = 6000) -> SavableSummaryBufferMemoryWithDates:
+        llm = FakeListLLM(responses=[self.fake_summary])
+        return SavableSummaryBufferMemoryWithDates.load(
+            llm=llm, max_token_limit=token_limit,
+            memory_key="chat_history", return_messages=True,
+            save_path=self.save_path
+        )
+
     def test_memory(self):
-        memory = self.agent._load_short_term_memory(self.test_user_id)
+        memory = self.load_memory()
         self.add_test_messages(memory)
         self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, "hi")
 
     def test_memory_savable(self):
-        memory = self.agent._load_short_term_memory(self.test_user_id)
+        memory = self.load_memory()
         self.add_test_messages(memory)
-        self.agent._save_short_term_memory(self.test_user_id, memory)
-        memory = self.agent._load_short_term_memory(self.test_user_id)
+        memory = self.load_memory()
         self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, "hi")
 
     def test_memory_clearable(self):
-        memory = self.agent._load_short_term_memory(self.test_user_id)
+        memory = self.load_memory()
         self.add_test_messages(memory)
         memory.clear()
         self.assertEqual(memory.load_memory_variables({})["chat_history"], [])
-        memory = self.agent._load_short_term_memory(self.test_user_id)
+        self.assertEqual(memory.moving_summary_buffer, "")
+        self.assertEqual(memory.chat_memory.messages, [])
+        memory = self.load_memory()
         self.assertEqual(memory.load_memory_variables({})["chat_history"], [])
+        self.assertEqual(memory.moving_summary_buffer, "")
+        self.assertEqual(memory.chat_memory.messages, [])
 
     def test_memory_summarization(self):
-        memory = self.agent._load_short_term_memory(self.test_user_id)
-        memory.max_token_limit = 20
+        memory = self.load_memory(20)
         self.add_test_messages(memory)
-        self.assertNotEqual(memory.load_memory_variables({})["chat_history"][0].content, "hi")
+        self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, self.fake_summary)
 
-        self.agent._save_short_term_memory(self.test_user_id, memory)
-        memory_buffer = memory.load_memory_variables({})["chat_history"][0].content
-        memory = self.agent._load_short_term_memory(self.test_user_id)
-        self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, memory_buffer)
+    def test_memory_summarization_savable(self):
+        memory = self.load_memory(20)
+        self.add_test_messages(memory)
+        memory = self.load_memory(20)
+        self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, self.fake_summary)
 
     def tearDown(self) -> None:
         shutil.rmtree(self.save_path)
