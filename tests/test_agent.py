3f55dae4f23e7e36ad16bd0bad2d1ac1f4198dec
my_mac_dimitree54 <dimitree54@gmail.com>
2023-06-18 15:44:36 +0300

    update tests + tests for end detector

--- tests/test_agent.py
+++ tests/test_agent.py
@@ -5,12 +5,27 @@ from unittest import TestCase
 
 import tiktoken
 from dotenv import load_dotenv
+from langchain.base_language import BaseLanguageModel
+from langchain.chat_models import ChatOpenAI
 from langchain.llms import FakeListLLM
 from langchain.memory.chat_memory import BaseChatMemory
-from langchain.schema import HumanMessage
 
 from chat_end_detector import ChatEndDetector
 from memory import SavableSummaryBufferMemoryWithDates
+
+
+def add_test_messages(memory: BaseChatMemory):
+    memory.save_context({"input": "hi"}, {"output": "whats up"})
+    memory.save_context({"input": "not much you"}, {"output": "not much"})
+    memory.save_context({"input": "bye"}, {"output": "see you"})
+
+
+def load_memory(llm: BaseLanguageModel, save_path: str, token_limit: int = 6000) -> SavableSummaryBufferMemoryWithDates:
+    return SavableSummaryBufferMemoryWithDates.load(
+        llm=llm, max_token_limit=token_limit,
+        memory_key="chat_history", return_messages=True,
+        save_path=save_path
+    )
 
 
 class FakeListLLMTiktoken(FakeListLLM):
@@ -28,65 +43,52 @@ class TestMemory(TestCase):
         self.save_path = tempfile.mkdtemp()
         self.test_user_id = 0
         self.fake_summary = "blah blah blah"
-
-    @staticmethod
-    def add_test_messages(memory: BaseChatMemory):
-        memory.save_context({"input": "hi"}, {"output": "whats up"})
-        memory.save_context({"input": "not much you"}, {"output": "not much"})
-        memory.save_context({"input": "bye"}, {"output": "see you"})
-
-    def load_memory(self, token_limit: int = 6000) -> SavableSummaryBufferMemoryWithDates:
-        llm = FakeListLLMTiktoken(responses=[self.fake_summary])
-        return SavableSummaryBufferMemoryWithDates.load(
-            llm=llm, max_token_limit=token_limit,
-            memory_key="chat_history", return_messages=True,
-            save_path=self.save_path
-        )
+        self.llm = FakeListLLMTiktoken(responses=[self.fake_summary])
 
     def test_memory(self):
-        memory = self.load_memory()
-        self.add_test_messages(memory)
+        memory = load_memory(self.llm, self.save_path)
+        add_test_messages(memory)
         self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, "hi")
 
     def test_memory_savable(self):
-        memory = self.load_memory()
-        self.add_test_messages(memory)
-        memory = self.load_memory()
+        memory = load_memory(self.llm, self.save_path)
+        add_test_messages(memory)
+        memory = load_memory(self.llm, self.save_path)
         self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, "hi")
 
     def test_memory_clearable(self):
-        memory = self.load_memory()
-        self.add_test_messages(memory)
+        memory = load_memory(self.llm, self.save_path)
+        add_test_messages(memory)
         memory.clear()
         self.assertEqual(memory.load_memory_variables({})["chat_history"], [])
         self.assertEqual(memory.moving_summary_buffer, "")
         self.assertEqual(memory.chat_memory.messages, [])
-        memory = self.load_memory()
+        memory = load_memory(self.llm, self.save_path)
         self.assertEqual(memory.load_memory_variables({})["chat_history"], [])
         self.assertEqual(memory.moving_summary_buffer, "")
         self.assertEqual(memory.chat_memory.messages, [])
 
     def test_memory_summarization(self):
-        memory = self.load_memory(20)
-        self.add_test_messages(memory)
+        memory = load_memory(self.llm, self.save_path, 20)
+        add_test_messages(memory)
         self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, self.fake_summary)
 
     def test_memory_summarization_savable(self):
-        memory = self.load_memory(20)
-        self.add_test_messages(memory)
-        memory = self.load_memory(20)
+        memory = load_memory(self.llm, self.save_path, 20)
+        add_test_messages(memory)
+        memory = load_memory(self.llm, self.save_path, 20)
         self.assertEqual(memory.load_memory_variables({})["chat_history"][0].content, self.fake_summary)
 
     def test_date(self):
-        memory = self.load_memory()
-        self.add_test_messages(memory)
+        memory = load_memory(self.llm, self.save_path)
+        add_test_messages(memory)
         self.assertTrue("timestamp" in memory.load_memory_variables({})["chat_history"][0].additional_kwargs)
 
     def test_date_savable(self):
-        memory = self.load_memory()
-        self.add_test_messages(memory)
+        memory = load_memory(self.llm, self.save_path)
+        add_test_messages(memory)
         date = memory.load_memory_variables({})["chat_history"][0].additional_kwargs["timestamp"]
-        memory = self.load_memory()
+        memory = load_memory(self.llm, self.save_path)
         self.assertEqual(memory.load_memory_variables({})["chat_history"][0].additional_kwargs["timestamp"], date)
 
     def tearDown(self) -> None:
@@ -95,11 +97,49 @@ class TestMemory(TestCase):
 
 class TestEndDetector(TestCase):
     def setUp(self) -> None:
+        load_dotenv()
+        self.save_path = tempfile.mkdtemp()
         self.end_detector = ChatEndDetector()
-        self.chat_history = [
-            HumanMessage(content="hi", additional_kwargs={"timestamp": 0}),
-        ]
+        self.llm = ChatOpenAI(model_name="gpt-3.5-turbo-0613", temperature=0)
 
-    def test_end_detection(self):
-        end_detector = ChatEndDetector()
+    def test_end1(self):
+        memory = load_memory(self.llm, self.save_path)
+        add_test_messages(memory)
+        new_message = "What is the weather today?"
+        is_new_conversation = self.end_detector.is_new_conversation(memory, new_message)
+        self.assertTrue(is_new_conversation)
 
+    def test_end2(self):
+        memory = load_memory(self.llm, self.save_path)
+        add_test_messages(memory)
+        new_message = "hi"
+        is_new_conversation = self.end_detector.is_new_conversation(memory, new_message)
+        self.assertTrue(is_new_conversation)
+
+    def test_continue1(self):
+        memory = load_memory(self.llm, self.save_path)
+        memory.save_context({"input": "hi"}, {"output": "whats up"})
+        memory.save_context({"input": "not much you"}, {"output": "not much"})
+        new_message = "bye"
+        is_new_conversation = self.end_detector.is_new_conversation(memory, new_message)
+        self.assertFalse(is_new_conversation)
+
+    def test_continue2(self):
+        memory = load_memory(self.llm, self.save_path)
+        memory.save_context({"input": "hi"}, {"output": "whats up"})
+        memory.save_context({"input": "not much you"}, {"output": "not much"})
+        new_message = "What is the weather today?"
+        is_new_conversation = self.end_detector.is_new_conversation(memory, new_message)
+        self.assertFalse(is_new_conversation)
+
+    def test_end3(self):
+        memory = load_memory(self.llm, self.save_path)
+        memory.save_context({"input": "hi"}, {"output": "whats up"})
+        memory.save_context({"input": "not much you"}, {"output": "not much"})
+        new_message = "hi"
+        self.end_detector._get_hours_after_message = lambda x: 26
+        is_new_conversation = self.end_detector.is_new_conversation(memory, new_message)
+        self.assertTrue(is_new_conversation)
+
+    def tearDown(self) -> None:
+        shutil.rmtree(self.save_path)
