987293150cbb83f4d25a5d106b5f26866decf977
my_mac_dimitree54 <dimitree54@gmail.com>
2023-07-01 00:39:45 +0300

    Keeping info about user always in memory

--- agents/stm_savable.py
+++ agents/stm_savable.py
@@ -3,14 +3,83 @@ import os
 from datetime import datetime
 from typing import List, Dict, Any
 
+from langchain.base_language import BaseLanguageModel
 from langchain.memory import ConversationSummaryBufferMemory, ChatMessageHistory
+from langchain.memory.summary import SummarizerMixin
+from langchain.prompts import ChatPromptTemplate
 from langchain.schema import BaseMessage, messages_from_dict, messages_to_dict
+from pydantic import BaseModel
 
 
 class ChatMessageHistoryWithDates(ChatMessageHistory):
     def add_message(self, message: BaseMessage) -> None:
         message.additional_kwargs["timestamp"] = datetime.now().isoformat()
         super().add_message(message)
+
+
+def _load_summary_memory(summary_memory_path: str) -> str:
+    if os.path.isfile(summary_memory_path):
+        with open(summary_memory_path, "r") as f:
+            summary_memory = f.read()
+    else:
+        summary_memory = ""
+    return summary_memory
+
+
+def _load_messages(save_path: str) -> List[BaseMessage]:
+    with open(save_path, "r") as f:
+        items = json.load(f)
+    messages = messages_from_dict(items)
+    return messages
+
+
+def _load_chat_memory_with_dates(chat_memory_path: str) -> ChatMessageHistoryWithDates:
+    if os.path.isfile(chat_memory_path):
+        messages = _load_messages(chat_memory_path)
+        chat_memory = ChatMessageHistoryWithDates(messages=messages)
+    else:
+        chat_memory = ChatMessageHistoryWithDates()
+    return chat_memory
+
+
+class SavableVeryImportantMemory(BaseModel):
+    summariser: SummarizerMixin
+    save_path: str
+    very_important_memory_file_name: str
+    buffer: str = ""
+
+    @classmethod
+    def load(cls, llm: BaseLanguageModel, save_path: str, summarizer_prompt: ChatPromptTemplate, **kwargs):
+        very_important_memory_file_name = kwargs.pop("very_important_memory_file_name", "very_important_memory.txt")
+        very_important_memory_path = os.path.join(save_path, very_important_memory_file_name)
+        if os.path.isfile(very_important_memory_path):
+            with open(very_important_memory_path, "r") as f:
+                buffer = f.read()
+        else:
+            buffer = ""
+        summariser = SummarizerMixin(
+            llm=llm,
+            prompt=summarizer_prompt
+        )
+        return cls(
+            summariser=summariser,
+            save_path=save_path,
+            very_important_memory_file_name=very_important_memory_file_name,
+            buffer=buffer, **kwargs
+        )
+
+    def _save(self):
+        very_important_memory_path = os.path.join(self.save_path, self.very_important_memory_file_name)
+        with open(very_important_memory_path, "w") as f:
+            f.write(self.buffer)
+
+    def update(self, new_messages: List[BaseMessage]):
+        self.buffer = self.summariser.predict_new_summary(new_messages, self.buffer)
+        self._save()
+
+    def clear(self):
+        self.buffer = ""
+        self._save()
 
 
 class SavableSummaryBufferMemoryWithDates(ConversationSummaryBufferMemory):
@@ -21,42 +90,17 @@ class SavableSummaryBufferMemoryWithDates(ConversationSummaryBufferMemory):
     @classmethod
     def load(cls, save_path: str, **kwargs):
         chat_memory_file_name = kwargs.pop("chat_memory_file_name", "chat_memory.json")
-        summary_memory_file_name = kwargs.pop("summary_memory_file_name", "summary_memory.json")
+        summary_memory_file_name = kwargs.pop("summary_memory_file_name", "summary_memory.txt")
         chat_memory_path = os.path.join(save_path, chat_memory_file_name)
         summary_memory_path = os.path.join(save_path, summary_memory_file_name)
-        chat_memory = cls._load_chat_memory(chat_memory_path)
-        summary_memory = cls._load_summary_memory(summary_memory_path)
+        chat_memory = _load_chat_memory_with_dates(chat_memory_path)
+        summary_memory = _load_summary_memory(summary_memory_path)
         return cls(
             save_path=save_path,
             chat_memory_file_name=chat_memory_file_name,
             summary_memory_file_name=summary_memory_file_name,
             chat_memory=chat_memory, moving_summary_buffer=summary_memory, **kwargs
         )
-
-    @staticmethod
-    def _load_messages(save_path: str) -> List[BaseMessage]:
-        with open(save_path, "r") as f:
-            items = json.load(f)
-        messages = messages_from_dict(items)
-        return messages
-
-    @staticmethod
-    def _load_chat_memory(chat_memory_path: str) -> ChatMessageHistoryWithDates:
-        if os.path.isfile(chat_memory_path):
-            messages = SavableSummaryBufferMemoryWithDates._load_messages(chat_memory_path)
-            chat_memory = ChatMessageHistoryWithDates(messages=messages)
-        else:
-            chat_memory = ChatMessageHistoryWithDates()
-        return chat_memory
-
-    @staticmethod
-    def _load_summary_memory(summary_memory_path: str) -> str:
-        if os.path.isfile(summary_memory_path):
-            with open(summary_memory_path, "r") as f:
-                summary_memory = f.read()
-        else:
-            summary_memory = ""
-        return summary_memory
 
     @staticmethod
     def _save_messages(messages: List[BaseMessage], save_path: str):
